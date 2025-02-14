4ab39047ed1e4ad39e867ba0a71c935ba8d705f6
Mykyta Bohdan <justkexitmail@gmail.com>
2025-02-14 23:19:46 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -31,15 +31,16 @@ class CarWashStation:
         return (round(income, 1))
 
     def calculate_washing_price(self, car: Car) -> float:
-        return (car.comfort_class * (self.clean_power - car.clean_mark)
-                * self.average_rating / self.distance_from_city_center)
+        if self.distance_from_city > 0:
+            return (car.comfort_class * (self.clean_power - car.clean_mark)
+                    * self.average_rating / self.distance_from_city_center)
 
     def wash_single_car(self, car: Car) -> None:
         car.clean_mark = self.clean_power
 
-    def rate_service(self, raiting: int) -> None:
+    def rate_service(self, rating: int) -> None:
         self.average_rating = round(
-            ((self.average_rating * self.count_of_ratings) + raiting)
+            ((self.average_rating * self.count_of_ratings) + rating)
             / (self.count_of_ratings + 1), 1
         )
         self.count_of_ratings += 1
